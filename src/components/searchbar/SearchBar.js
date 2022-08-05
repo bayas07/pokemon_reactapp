e5f621edeb906098e1e067bbab1aa58a3221009b
bayas07 <bayasahamed19@gmail.com>
2022-08-05 08:37:26 +0530

    Action handlers are added in search bar

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState();
+
+  const onChangeHandler = (event) => {
+    event.preventDefault();
+    setSearchInput(event.target.value);
+  };
+  const onKeyPressHandler = ({ key }) => {
+    if (key === "Enter") {
+      console.log("Search completed");
+    }
+  };
+  const searchHandler = () => {
+    console.log("Search completed");
+  };
+  console.log(searchInput, "searchInput");
   return (
-    <form>
-      <div className="searchbar-container">
+    <div className="searchbar-container">
         <input
           type="text"
           aria-label="search"
           placeholder="Search your pokemon here"
+          onChange={onChangeHandler}
+          onKeyPress={onKeyPressHandler}
         />
-        <FontAwesomeIcon icon={faSearch} className="search-icon" />
-      </div>
-    </form>
+        <FontAwesomeIcon
+          icon={faSearch}
+          className="search-icon"
+          onClick={searchHandler}
+        />
+    </div>
   );
 };
 
